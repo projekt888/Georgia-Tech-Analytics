64adecaf52c423ef0bfb0e19487ae07b36267366
projekt888 <roberts888@gmail.com>
2019-08-24 10:40:50 -0400

    changed PyBank to use next(header)

--- HW3/python-challenge/PyBank/main.py
+++ HW3/python-challenge/PyBank/main.py
@@ -12,29 +12,28 @@ def print_output(months, net_total_profits, avg, month_max,max,month_min,min): #
     output_string+=f"Greatest Decrease in Profits: {month_min} (${min:.0f})"+'\n'
     return(output_string)
 
-with open ('budget_data.csv') as csv_file: #reads the file
+with open ('budget_data.csv') as csv_file: #reads the file, assumes it's in the same directory
     csv_reader = csv.reader(csv_file, delimiter=',')
-    lines = 0
     months = 0
     last_month = 0
     this_month = 0
     changes = [] # deltas (how much money did we make/lose)
     months_a = [] #a parallel array with the months.  The parallel array lets me use the max and min functions
     net_total_profits = 0
+
+    row = next(csv_reader) #skip the header
     for row in csv_reader:
-            #skip the header
-            lines+=1
-            if (lines >1):
-                months+=1
-                net_total_profits += float(row[1])
 
-                #dont record the first month as a change
-                if (lines>2):
-                    this_month = float(row[1])
-                    changes.append(this_month - last_month)
-                    months_a.append(str(row[0]))
+        months+=1
+        net_total_profits += float(row[1])
 
-                last_month = float(row[1])
+        if (months>1):
+            #dont record the first month as a change
+            this_month = float(row[1])
+            changes.append(this_month - last_month)
+            months_a.append(str(row[0]))
+
+        last_month = float(row[1])
 
     print(print_output(months, net_total_profits, statistics.mean(changes),months_a[changes.index(max(changes))],max(changes),months_a[changes.index(min(changes))],min(changes)))
     ### NOW WRITE THIS TO A file
